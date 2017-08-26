14eb00f897adc87a855d6be0ff06ff21c5acc72e
aleapoorv <30747846+aleapoorv@users.noreply.github.com>
2017-08-26 12:29:54 +0530

    Add files via upload

---
+++ abc.java
@@ -0,0 +1,9 @@
+class abc{
+public static void main(String args[]){
+
+String jumbledWords[]={"alpep","argneo","rgarps"};
+for(int i=0;i<jumbledWords.length;i++)
+System.out.println(jumbledWords[i]);
+
+}
+}
---
+++ abcd.java
@@ -0,0 +1,14 @@
+class abcd{
+public static void main(String args[]){
+
+String [] [] jumbledWords= new String [] [] {
+{"alpep","argneo"},{"asfhasf","asdasd"},{"asdads","asdasda"}};
+System.out.println("Elements stored in array are:");
+for(int i=0;i<2;i++)
+{
+for(int j=0;j<2;j++)    { 
+System.out.println(jumbledWords[i][j]);
+}
+}
+}
+}
